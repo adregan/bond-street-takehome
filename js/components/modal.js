347f6ad8e210d3aa27e7bbd019e84516dfa164db
Duncan Regan <duncanregan@gmail.com>
2016-03-29 15:51:56 -0400

    building up the modal component

--- js/components/modal.js
+++ js/components/modal.js
@@ -7,7 +7,17 @@ class Modal extends React.Component {
   render () {
     return (
       <div className="modal-container">
-        <article className="modal" />
+        <article className="modal">
+          <header className="modal__header">
+            <h2 className="modal__title"></h2>
+            <p className="modal__description">Enter your credentials to link your account</p>
+          </header>
+          <form className="modal__form">
+            <input className="modal__input" />
+            <input className="modal__input" />
+            <button type="submit" className="modal__submit"/>
+          </form>
+        </article>
       </div>
     );
   }
