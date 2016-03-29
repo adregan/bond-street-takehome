30d71b5e2fddbeea68c293ad092dc9719914b690
Duncan Regan <duncanregan@gmail.com>
2016-03-29 15:04:48 -0400

    making the component more presentational

--- js/components/connectToBank.js
+++ js/components/connectToBank.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { institutions } from '../../config';
 import { setInstitution } from '../actions';
+import Bank from './bank';
 
-const ConnectToBank = ({ dispatch }) => {
+const ConnectToBank = ({ banks, dispatch }) => {
   return (
     <section className="connect">
       <p>Connecting us to your primary business bank account gives us a clearer understanding of your finances.</p>
       <ul className="banks-list">
-        {institutions.map((institution, i) => {
-          return (
-            <li className="banks-list__item" key={i} onClick={() => dispatch(setInstitution(institution.id))}>
-              {institution.name}
-            </li>
-            );
+        {banks.map((bank, i) => {
+          return <Bank key={i} bank={bank} handleClick={(id) => dispatch(setInstitution(id))} />;
         })}
       </ul>
     </section>
