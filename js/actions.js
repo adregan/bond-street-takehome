9ad74c3bd8b9ef66ddb6cbb6aaa4f0b1d0a66dfd
Duncan Regan <duncanregan@gmail.com>
2016-03-28 22:46:23 -0400

    adds actions

---
+++ js/actions.js
@@ -0,0 +1,44 @@
+import { routes as ROUTES } from '../config';
+
+/* ROUTE ACTION TYPES & ACTIONS */
+export const CHANGE_ROUTE = 'CHANGE_ROUTE';
+
+export const changeRoute = (route) => {
+  if (ROUTES.includes(route)) {
+    return {type: CHANGE_ROUTE, route};
+  }
+};
+
+
+/* INSTITUTION ACTION TYPES & ACTIONS */
+export const SET_INSTITUTION = 'SET_INSTITUTION';
+
+export const setInstitution = (id) => {
+  return {type: SET_INSTITUTION, id};
+};
+
+/* LOGIN ERROR ACTION TYPES & ACTIONS */
+export const CLEAR_ERROR = 'CLEAR_ERROR';
+export const DISPLAY_ERROR = 'DISPLAY_ERROR';
+
+export const clearError = () => {
+  return {type: CLEAR_ERROR};
+};
+
+export const displayError = (msg) => {
+  return {type: DISPLAY_ERROR, msg};
+};
+
+
+/* LOADING ACTION TYPES & ACTIONS */
+export const LOADING = 'LOADING';
+export const LOADED = 'LOADED';
+
+export const loading = () => {
+  return {type: LOADING};
+};
+
+export const loaded = () => {
+  return {type: LOADED};
+};
+
