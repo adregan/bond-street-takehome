f4fd92269fbcd2ac3e6cd33abbabc87c6f4285e6
Duncan Regan <duncanregan@gmail.com>
2016-03-29 20:55:56 -0400

    submitting form data actions from the modal

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { logIn, clearError } from '../actions';
 
 class Modal extends React.Component {
   constructor (props) {
     super(props);
   }
+  submitForm (event) {
+    event.preventDefault();
+    this.props.dispatch(clearError()); 
+    const formData = Array.prototype.filter.call(event.target.elements, el => el.tagName !== 'BUTTON')
+      .map(input => `${input.name}=${input.value}`)
+      .join('&');
+    if (this.props.state === 'login') {
+      this.props.dispatch(logIn(formData));
+    }
+  }
+  generateInputs (state, loginFields) {
+    if (state === 'login') {
+      return loginFields.map((field, i) => {
+        const type = (field.mask) ? 'password' : 'text';
+        return <input required={true} className="modal__input" key={i} type={type} placeholder={field.placeholder} name={field.name} />;
+      });
+    }
+    else {
+      return (
+        <span>
+          <input className="modal__input" placeholder="Account Number" />
+          <input className="modal__input" placeholder="Routing Number" />
+        </span>
+      );
+    }
+  }
   render () {
-    const { state, error, loading } = this.props;
+    const { state, error, loading, loginFields } = this.props;
     const title = (state === 'login') ? 'Log in to your bank account' : 'Enter your routing and account numbers';
+    const buttonText = (loading) ? 'Logging in...' : 'Continue';
     return (
       <div className="modal-container">
         <article className="modal">
